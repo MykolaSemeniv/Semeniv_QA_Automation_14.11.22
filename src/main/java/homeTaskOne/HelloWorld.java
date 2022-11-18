452216c0f2ded817aa02335a74b88d4ffefa98a4
MykolaSemeniv <nick.sem@ukr.net>
2022-11-18 19:35:56 +0200

    upd for homeTaskOne

--- src/main/java/homeTaskOne/HelloWorld.java
+++ src/main/java/homeTaskOne/HelloWorld.java
@@ -5,7 +5,9 @@ public class HelloWorld {
     {
         String text1 = "Hello world!";
         String text2 = "java";
+        String text3 = "Third";
         System.out.println(text1);
         System.out.println(text2);
+        System.out.println(text3);
     }
 }
