1606394fa7f7a086aef556169b488833a3dde264
MykolaSemeniv <nick.sem@ukr.net>
2022-11-18 19:02:50 +0200

    lesson2

---
+++ src/main/java/homeTaskOne/HelloWorld.java
@@ -0,0 +1,9 @@
+package homeTaskOne;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        String text = "Hello world!";
+        System.out.println(text);
+    }
+}
